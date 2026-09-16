b162182fb2fc1ec79127a2dd87278f25f8594c87
Nini's Mac <mac@ninni.local>
2026-09-16 22:03:30 +0400

    second lecture

---
+++ Lecture 01/main.py
@@ -0,0 +1,117 @@
+######################################
+# Integers & Floats / int/float
+######################################
+
+print(10)
+print(10.5)
+
+print(type(10))
+print(type(10.5))
+
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+
+print(10 + 5.4)
+print(10.5 - 5.4)
+print(10 * 5.4)
+
+print(10 / 2)
+
+print(10 // 3)
+
+print(10 % 3)
+
+print(10 ** 3)
+
+# -b / (2 * a)
+
+print(0.1 + 0.3)
+
+print(0.1 + 0.2)
+
+print(5 % 2)
+print(5 % -2)
+print(-5 % 2)
+
+print(5 // 2) # 2.5
+print(5 // -2) # -2.5
+
+
+
+######################################
+# Strings / str
+######################################
+
+print(type("Python"))
+print('Python', 'Java')
+
+
+
+######################################
+# Boolean / bool
+######################################
+
+print(True)
+print(False)
+
+print(10 > 5)
+print(10 < 5)
+print(10 == 5)
+print(10 != 5)
+print(10 >= 5)
+print(10 <= 5)
+
+
+
+
+######################################
+# NoneType / None
+######################################
+
+print(None)
+print(type(None))
+
+
+
+######################################
+# Variables
+######################################
+
+num1 = 5
+num2 = 2
+
+print(num1 + num2)
+print(num1 - num2)
+
+count = 3
+print(count)
+# count = count - 1
+count -= 1
+print(count)
+
+
+user_age = 10 # snake_case
+userAge = 20 # camelCase
+
+Num1 = 10 # PascalCase
+
+import keyword
+print(keyword.kwlist)
+
+
+
+######################################
+# Casting
+######################################
+
+num1 = int(input("Enter a num1: "))
+num2 = int(input("Enter a num2: "))
+
+# num1 = int(num1)
+# num2 = int(num2)
+
+print(type(num1))
+print(type(num2))
+
+print(num1 + num2)
